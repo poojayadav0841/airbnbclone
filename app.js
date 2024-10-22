4904d634e133cad0f469f764fca5d1ff18b57e7a
poojayadav0841 <252poojayadav@gmail.com>
2024-10-22 13:26:00 +0530

    main route

--- app.js
+++ app.js
@@ -70,9 +70,9 @@ const sessionOptions={
     }
 };
 
-// app.get("/",(req,res)=>{
-//     res.send("Hi, I'm root");
-// })
+app.get("/",(req,res)=>{
+    res.redirect("/listings");
+})
 
 
 app.use(session(sessionOptions));
